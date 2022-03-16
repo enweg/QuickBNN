0b85b516466d4fbe71415f8e225f1b446f678b43
Enrico Wegner <wegner1996@gmail.com>
2022-03-16 21:57:29 +0100

    chaging include order

--- src/QuickBNN.jl
+++ src/QuickBNN.jl
@@ -18,13 +18,13 @@ using Bijectors
 include("utils.jl")
 include("NNAR.jl")
 include("AR.jl")
-include("model.jl")
 include("./layers/dense.jl")
+include("model.jl")
 
 ################################################################################
 # Exports
 ################################################################################
-export DenseBNN, DenseOrderedBias, DenseOrderedWeights, DenseForcePosFirstWeight
+export DenseBNN, DenseOrderedBias, DenseOrderedWeights, DenseForcePosFirstWeight, ChainBNN
 export NNAR, theoretical_dist
 export make_BNN, generated_quantities_chain, posterior_predictive, BNN
 export AR, theoretical_dist
